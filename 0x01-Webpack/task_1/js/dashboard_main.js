c05c57b247fa624e775ce340c5dc114b5e6258d1
Corbin <forstupidityonly@gmail.com>
2022-02-03 16:23:01 -0600

    moved below

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,10 +7,10 @@ $('body').append('<button type="button">Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter, 150));
-
 let count = 0;
 function updateCounter() {
   count += 1;
   $('#count').html(`${count} clicks on the button`);
 };
+
+$('button').on('click', _.debounce(updateCounter, 150));
