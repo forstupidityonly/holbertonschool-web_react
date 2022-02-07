87e39439a75c8ab88f684cc9e1b965fe5db5ecab
Corbin <forstupidityonly@gmail.com>
2022-02-07 12:55:15 -0600

    pushing changes b4 doctors apt

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.jpg';
+import './App.css';
+import {getFullYear, getFooterCopy} from './utils.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <div className="form">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" className="inputForm"></input>
+          <label for="password">Password:</label>
+          <input type="password" id="password" name="password" className="inputForm"></input>
+          <button type="button">OK</button>
+        </div>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
