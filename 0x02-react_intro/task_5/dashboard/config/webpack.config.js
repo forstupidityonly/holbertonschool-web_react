fa528c0b248589c2508aec1dee9e809b028408b2
Corbin <forstupidityonly@gmail.com>
2022-02-09 13:39:46 -0600

    jasmine said add babel

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        loader: ‘babel-loader’,
+      },
+      {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           'file-loader',
