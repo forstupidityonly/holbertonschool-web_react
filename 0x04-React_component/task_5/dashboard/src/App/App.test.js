9ae0c6f2ad63293bd8920f57d13cfc43604bf241
Corbin <forstupidityonly@gmail.com>
2022-02-24 12:44:34 -0600

    looks like all my files wernt pussed

---
+++ 0x04-React_component/task_5/dashboard/src/App/App.test.js
@@ -0,0 +1,59 @@
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import App from './App';
+import Notifications from '../Notifications/Notifications.js';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import Footer from '../Footer/Footer.js';
+import CourseList from '../CourseList/CourseList.js';
+import '../../config/setupTests';
+
+describe('Test App', () => {
+
+  it('renders App without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders Notifications without crashing', () => {
+    shallow(<Notifications />);
+  });
+
+  it('renders Header  without crashing', () => {
+    shallow(<Header />);
+  });
+
+  it('renders Login without crashing', () => {
+    shallow(<Login />);
+  });
+
+  it('renders Footer  without crashing', () => {
+    shallow(<Footer />);
+  });
+
+  it('check that CourseList is not displayed', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login)).toHaveLength(1);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+  });
+
+  it('check that CourseList is displayed', () => {
+    const wrapper = shallow(<App isLoggedIn={true}/>);
+    expect(wrapper.find(Login)).toHaveLength(0);
+    expect(wrapper.find(CourseList)).toHaveLength(1);
+  });
+
+  it('ctr + h calls logout', () => {
+    const events = {};
+    const logout = jest.fn();
+    document.addEventListener = jest.fn((event, cb) => {
+      events[event] = cb;
+    });
+    window.alert = jest.fn();
+    shallow(<App logOut={logout} />);
+    events.keydown({key: "h", ctrlKey: true});
+    expect(window.alert).toHaveBeenCalledWith("Logging you out");
+    expect(logout).toHaveBeenCalled();
+    jest.restoreAllMocks();
+  });
+
+});
---
+++ 0x04-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './Notifications.css';
+import NotificationItem from './NotificationItem.js';
+import NotificationItemShape from './NotificationItemShape.js';
+import closeIcon from '../assets/close-icon.png';
+import {getLatestNotification} from '../utils/utils.js';
+import PropTypes from 'prop-types';
+
+class Notifications extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  };
+
+  shouldComponentUpdate(nextProps) {
+    // Rendering the component only if
+    // passed props value is changed
+
+    if (nextProps.listNotifications.length > this.props.listNotifications.length) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  render () {
+    return (
+      <React.Fragment>
+        <div className="menuItem">Your notifications</div>
+        { this.props.displayDrawer && (
+          <div className="Notifications">
+            { this.props.listNotifications.length > 0 ? (
+              <React.Fragment>
+                <p style={{ display: "inline" }}>
+                  Here is the list of notifications
+                </p>
+                <button
+                  onClick={() => {
+                  console.log('Close button has been clicked');
+                  }}
+                  style={{
+                    float: 'right',
+                    height: '35px',
+                    width: '35px',
+                    position: 'absolute',
+                    top: '20px',
+                    right: '20px',
+                    background: 'none',
+                    border: 'none',
+                  }}
+                  aria-label="Close"
+                  ><img
+                  src={closeIcon}
+                  style={{
+                  height: '30px',
+                  width: '30px',
+                  }} alt="Close"/></button>
+                  <ul>
+                    {this.props.listNotifications.map((Notification) => (
+                      <NotificationItem 
+                      markAsRead={this.markAsRead}
+                      key={Notification.id}
+                      id={Notification.id}
+                      type={Notification.type}
+                      value={Notification.value} 
+                      html={Notification.html} />
+                    ))}
+                 </ul>
+                 </React.Fragment>
+           ) : <p>No new notification for now</p>}
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+export default Notifications;
