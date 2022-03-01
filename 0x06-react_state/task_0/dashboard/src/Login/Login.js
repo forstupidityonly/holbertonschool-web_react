1de1f88572ff5491299646c93cd4ce01426cb6d2
Corbin <forstupidityonly@gmail.com>
2022-03-01 13:44:33 -0600

    pushing so i can see files while writing tests for login

--- 0x06-react_state/task_0/dashboard/src/Login/Login.js
+++ 0x06-react_state/task_0/dashboard/src/Login/Login.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import {getFullYear, getFooterCopy} from '../utils/utils.js';
 import { StyleSheet, css } from 'aphrodite';
+import PropTypes from 'prop-types';
 
-function Login() {
-  return (
-    <div className={css(styles.Login)}>
-      <p>Login to access the full dashboard</p>
-      <div className="form">
-        <React.Fragment>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" className="inputForm"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" className="inputForm"></input>
-          <button type="button">OK</button>
-        </React.Fragment>
-      </div>
-    </div>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoggedIn: false};
+  };
+
+  render () {
+    return (
+      <React.Fragment>
+        <div className={css(styles.Login)}>
+          <p>Login to access the full dashboard</p>
+          <div className="form">
+            <React.Fragment>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" className="inputForm"></input>
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" className="inputForm"></input>
+              <button type="button">OK</button>
+            </React.Fragment>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -28,5 +38,11 @@ const styles = StyleSheet.create({
     marginLeft: 20,
   },
 });
+
+Login.propTypes = {
+};
+
+Login.defaultProps = {
+};
 
 export default Login;
