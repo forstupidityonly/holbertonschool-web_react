153462632dbf22315e7fc47afbbd0895bf8c93df
Corbin <forstupidityonly@gmail.com>
2022-03-01 14:16:26 -0600

    compleated login tests

--- 0x06-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x06-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -16,12 +16,26 @@ describe('Test Login', () => {
 
   it('verify that Login renders three input items', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find('input')).toHaveLength(2);
+    expect(wrapper.find('input')).toHaveLength(3);
   });
 
   it('verify that Login renders three label items', () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.find('label')).toHaveLength(2);
+  });
+
+  it('verify submit button is disabled by default', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find('input#submit').is('[disabled]')).toBe(true);
+  });
+
+  it('verify changing email & password enables button', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find('input#email')).toHaveLength(1);
+    expect(wrapper.find('input#password')).toHaveLength(1);
+    wrapper.find('input#email').simulate('change', {target: { value: 'some@email.com' }});
+    wrapper.find('input#password').simulate('change', {target: { value: 'password' }});
+    expect(wrapper.state().enableSubmit).toBe(true);
   });
 
 });
