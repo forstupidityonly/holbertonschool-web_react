635b63f0163b6ebf0d08c0b70acd18428c223e4d
Corbin <forstupidityonly@gmail.com>
2022-03-08 14:40:58 -0600

    I hope to god this is right bc i am so very done with immutable obj

---
+++ 0x07-React_Immutable/8-main.js
@@ -0,0 +1,21 @@
+import printBestStudents from './8-seq.js';
+
+const grades = {
+  1: {
+    score: 99,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  },
+  2: {
+    score: 70,
+    firstName: 'Corbin',
+    lastName: 'VanDeventer',
+  },
+  3: {
+    score: 50,
+    firstName: 'John',
+    lastName: 'Doe',
+  },
+};
+
+printBestStudents(grades);
--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,5 +1,15 @@
 import { Seq } from 'immutable';
 
-export function printBestStudents(obj){
-  return ();
-}
+export default function printBestStudents(obj) {
+  const mySeq = Seq(obj);
+  const myStuPassing = mySeq.filter((value) => value.score > 70);
+  const myStu = myStuPassing.toJS();
+  const toUpper = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(myStu).map((key) => {
+    myStu[key].firstName = toUpper(myStu[key].firstName);
+    myStu[key].lastName = toUpper(myStu[key].lastName);
+    return myStu[key];
+  });
+  console.log(myStu);
+};
