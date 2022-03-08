89d4c65458b2d99de5ed94e84161ce3968eb936f
Corbin <forstupidityonly@gmail.com>
2022-03-08 13:31:14 -0600

    cpy pst spacing issue

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,12 +1,12 @@
 import { List, Map } from 'immutable';
 
 const map = Map({
-     1: 'Liam',
-     2: 'Noah',
-     3: 'Elijah',
-     4: 'Oliver',
-     5: 'Jacob',
-     6: 'Lucas',
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
 });
 
 const map2 = List(map).withMutations((tempMap) => {
